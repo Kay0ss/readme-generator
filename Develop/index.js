4d45bb64ca0cc1255d053a7bc3a4ce0513116e31
Kay Oss <mne36325i@gmail.com>
2021-10-16 15:07:03 -0500

    completed questions

--- Develop/index.js
+++ Develop/index.js
@@ -11,37 +11,50 @@ inquirer
     },
     {
       type: "input",
-      message: "What is a short description of the project?",
+      message: "Provide a short description explaining the what, why, and how of your project.",
       name: "description",
     },
     {
       type: "input",
-      message: "what do you want in your bio?",
-      name: "bio",
+      message: "Please provide a step-by-step description of how to get the development environment running.",
+      name: "installation",
     },
     {
       type: "input",
-      message: "what is your Linkedin URL?",
-      name: "linkedin",
+      message: "What are the instructions for use?",
+      name: "usage",
     },
     {
       type: "input",
-      message: "what is your Github URL?",
-      name: "github",
+      message: "List your collaborators, with links to their Github profiles",
+      name: "credits",
+    },    
+    {
+        type: "input",
+        message: "What is your license type for the project? If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/)",
+        name: "license",
     },
+    
   ])
 
   .then((response) => {
     const projectTitle = response.title;
     console.log(projectTitle);
+
     const projDescription = response.description;
     console.log(projDescription);
-    const bio = response.bio;
-    console.log(bio);
-    const linkedin = response.linkedin;
-    console.log(linkedin);
-    const github = response.github;
-    console.log(github);
+
+    const install = response.installation;
+    console.log(install);
+
+    const usageNotes = response.usage;
+    console.log(usageNotes);
+
+    const credit = response.credits;
+    console.log(credit);
+
+    const licenseInfo = response.license;
+    console.log(licenseInfo);
 
     const temp = generateMarkdown(response);
 
@@ -52,14 +65,22 @@ inquirer
 # Description
 ## ${projDescription}
 
-# Bio
-## ${bio}
+# Table of Contents
+## 
 
-# Linkedin
-## ${linkedin}
+# Installation
+## ${install}
 
-# Github
-## ${github}
+# Usage
+## ${usageNotes}
+
+# Credits
+## ${credit}
+
+# License
+## ${licenseInfo}
+
+
     
     `;
 
